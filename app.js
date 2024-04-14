67345ff36134cb01723fbb2c5ed9ae66ca4c2051
emil6957 <emiliano.milewski@gmail.com>
2024-04-14 21:20:17 +0100

    x

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var carsRouter = require("./routes/cars");
+var carsRouter = require("./routes/cars.js");
 
 var app = express();
 
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/cars", carsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
