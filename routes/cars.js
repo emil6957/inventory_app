507af5da1f89b2cc694a0fa1ddd34a69ab9c4d24
emil6957 <emiliano.milewski@gmail.com>
2024-04-14 21:59:44 +0100

    added manufacturer to route and tested

--- routes/cars.js
+++ routes/cars.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const car_controller = require("../controllers/carController.js");
+const manufacturer_controller = require("../controllers/manufacturerController.js");
 
 router.get("/", car_controller.index);
 
@@ -16,5 +17,19 @@ router.post("/car/:id/update", car_controller.car_update_post);
 
 router.get("/car/:id", car_controller.car_detail);
 router.get("/cars", car_controller.car_list);
+
+router.get("/", manufacturer_controller.index);
+
+router.get("/manufacturer/create", manufacturer_controller.manufacturer_create_get);
+router.post("/manufacturer/create", manufacturer_controller.manufacturer_create_post);
+
+router.get("/manufacturer/:id/delete", manufacturer_controller.manufacturer_delete_get);
+router.post("/manufacturer/:id/delete", manufacturer_controller.manufacturer_delete_post);
+
+router.get("/manufacturer/:id/update", manufacturer_controller.manufacturer_update_get);
+router.post("/manufacturer/:id/update", manufacturer_controller.manufacturer_update_post);
+
+router.get("/manufacturer/:id", manufacturer_controller.manufacturer_detail);
+router.get("/manufacturers", manufacturer_controller.manufacturer_list);
 
 module.exports = router;
