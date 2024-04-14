9260832f8ff6c8b34e6a3b1fa090d3f22b836698
emil6957 <emiliano.milewski@gmail.com>
2024-04-14 21:58:25 +0100

    small change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var carsRouter = require("./routes/cars.js");
+const carsRouter = require("./routes/cars");
 
 var app = express();
 
