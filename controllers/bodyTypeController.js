a8e5fc0832ec92af688c9dfc46c633233896a649
emil6957 <emiliano.milewski@gmail.com>
2024-04-16 15:35:14 +0100

    made views of bodytype detail and list also rendered them in the controller

--- controllers/bodyTypeController.js
+++ controllers/bodyTypeController.js
@@ -1,4 +1,5 @@
 const BodyType = require("../models/bodyType.js");
+const Car = require("../models/car.js");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
@@ -6,15 +7,18 @@ exports.index = asyncHandler(async (req, res, next) => {
 });
 
 exports.bodyType_list = asyncHandler(async (req, res, next) => {
-    res.send("TODO bodType list");
+    const bodyType_list = await BodyType.find({})
+        .sort({ type: 1 })
+        .exec();
+    res.render("bodyType_list", { allBodyTypes: bodyType_list })
 });
 
 exports.bodyType_detail = asyncHandler(async (req, res, next) => {
-    res.send("TODO bodType detail");
-});
-
-exports.bodyType_detail = asyncHandler(async (req, res, next) => {
-    res.send("TODO bodType detail");
+    const type = req.params.id;
+    const bodyType_detail = await BodyType.findOne({ type: type })
+        .exec();
+    const cars = await Car.find({ bodyType: bodyType_detail._id });
+    res.render("bodyType_detail", { bodyType: bodyType_detail, cars: cars });
 });
 
 exports.bodyType_create_get = asyncHandler(async (req, res, next) => {
