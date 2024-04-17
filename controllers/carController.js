ce5c5a6a8be1ffae9f15f1fbf44d69c2639d9b93
emil6957 <emiliano.milewski@gmail.com>
2024-04-17 23:31:40 +0100

    incorporated car_form into get and post handlers

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require("express-validator");
 const Car = require("../models/car.js");
 const Manufacturer = require("../models/manufacturer.js");
 const BodyType = require("../models/bodyType.js");
@@ -43,12 +44,98 @@ exports.car_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.car_create_get = asyncHandler(async (req, res, next) => {
-    res.send("TODO car GET");
+    const [allManufacturers, allBodyTypes] = await Promise.all([
+        Manufacturer.find().sort({ name: 1 }).exec(),
+        BodyType.find().sort({ type: 1 }).exec(),
+    ]);
+
+    res.render("car_form", {
+        title: "Add new car",
+        manufacturers: allManufacturers,
+        bodyTypes: allBodyTypes,
+    });
 });
 
-exports.car_create_post = asyncHandler(async (req, res, next) => {
-    res.send("TODO car POST");
-});
+exports.car_create_post = [
+    (req, res, next) => {
+        if (!Array.isArray(req.body.bodyType)) {
+            req.body.bodyType =
+                typeof req.body.bodyType === "undefined" ? [] : [req.body.bodyType];
+        }
+        next();
+    },
+
+    body("name", "Name must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("manufacturer", "Manufacturer must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("productionStartYear", "productionStartYear must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("productionEndYear", "productionEndYear must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("amntInStock", "amntInStock must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("price", "price must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("bodyType.*").escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const car = new Car({
+            name: req.body.name,
+            manufacturer: req.body.manufacturer,
+            bodyType: req.body.bodyType,
+            productionStartYear: req.body.productionStartYear,
+            productionEndYear: req.body.productionEndYear,
+            price: req.body.price,
+            amntInStock: req.body.amntInStock,
+        });
+
+        if (!errors.isEmpty()) {
+
+            const [allManufacturers, allBodyTypes] = await Promise.all([
+                Manufacturer.find().sort({ name: 1 }).exec(),
+                BodyType.find().sort({ type: 1 }).exec(),
+            ]);
+
+            console.log("CAR BODYTYPE");
+            console.log(car.bodyType);
+            console.log("ALL BODY TYPES");
+            console.log(allBodyTypes);
+            for (const bodyType of allBodyTypes) {
+                console.log("SEPERATE BODY TYPE");
+                console.log(bodyType);
+                if (car.bodyType.includes(bodyType._id)) {
+                    bodyType.checked = "true";
+                }
+            }
+
+            res.render("car_form", {
+                name: "Create Car",
+                manufacturers: allManufacturers,
+                bodyTypes: allBodyTypes,
+                car: car,
+                errors: errors.array(),
+            });
+        } else {
+            await car.save();
+            res.redirect("./cars/cars");
+        }
+    }),
+];
 
 exports.car_delete_get = asyncHandler(async (req, res, next) => {
     res.send("TODO car delete GET");
