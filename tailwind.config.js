63ed69b5a0b6f41ad8bd86b64e444a2ed01a3b98
emil6957 <emiliano.milewski@gmail.com>
2024-04-21 19:28:43 +0100

    trying to fix it

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["*./src/**/*.{pug,html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
 
