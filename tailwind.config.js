1df63d467d4822e3b6661a69faf29634d3f694c5
emil6957 <emiliano.milewski@gmail.com>
2024-04-21 23:06:57 +0100

    added tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
 module.exports = {
-    content: ["./views/*.{pug,html}"],
+    content: ["./views/*.{pug,html}", "./views/includes/*.{pug,html}"],
     theme: {
+        colors: {
+            "white": "#FFFFFF",
+            "black": "#000103",
+            "red": "#EF2917",
+            "yellow": "#FFE548",
+            "blue": "4357AD",
+        },
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        plugin(function({ addBase, theme }) {
+            addBase({
+                html: { margin: theme("margin.0"), padding: theme("padding.0") },
+                body: { margin: theme("margin.0"), padding: theme("padding.0") },
+                h1: { fontSize: theme("fontSize.8xl") },
+            })
+        })
+    ],
 }
 
