2c4a5f1cf217c405556a312782a530136b164957
emil6957 <emiliano.milewski@gmail.com>
2024-04-23 23:11:33 +0100

    added new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,24 @@ const plugin = require("tailwindcss/plugin");
 module.exports = {
     content: ["./views/*.{pug,html}", "./views/includes/*.{pug,html}"],
     theme: {
-        colors: {
-            "white": "#FFFFFF",
-            "black": "#000103",
-            "red": "#EF2917",
-            "yellow": "#FFE548",
-            "blue": "4357AD",
+        extend: {
+            colors: {
+                "white": "#FFFFFF",
+                "black": "#000103",
+                "red": "#EF2917",
+                "yellow": "#FFE548",
+                "blue": "4357AD",
+                "gold": "#C8B568",
+            },
+            fontFamily: {
+                body: ["Lato"],
+            },
         },
-        extend: {},
     },
     plugins: [
         plugin(function({ addBase, theme }) {
             addBase({
+                "*": { fontFamily: ["Lato"] },
                 html: { margin: theme("margin.0"), padding: theme("padding.0") },
                 body: { margin: theme("margin.0"), padding: theme("padding.0") },
                 h1: { fontSize: theme("fontSize.8xl") },
